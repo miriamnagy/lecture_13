5dcc4672f8ab0c3996ad6b203a50eb3132a2d2fd
Miriam Nagyova <mimi.nagyova@gmail.com>
2021-05-03 10:44:13 +0200

    Add solution for fib. sequence

--- fibo.py
+++ fibo.py
@@ -1,7 +1,16 @@
+def recursive_nth_fibo(number):
+    if number <= 1:
+        return number
+    else:
+        return recursive_nth_fibo(number - 1 ) + recursive_nth_fibo(number - 2)
 
 def main():
-    pass
-
+    number = int(input("Zadaj cislo:"))
+    n_th = recursive_nth_fibo(number)
+    fib_seq = []
+    for num in range(number + 1):
+        fib_seq.append(recursive_nth_fibo(num))
+    print(fib_seq)
 
 if __name__ == '__main__':
     main()
